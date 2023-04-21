5ef26725315f6ddaf8a15b2aa82dfa9009999d35
RodrigoPaulaLopes <rodrigo.plopesti@gmail.com>
2023-04-21 17:38:06 -0300

    Create database  with typeorm and mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { ConfigModule } from '@nestjs/config';
+import dotenv from "dotenv"
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+  ConfigModule.forRoot(), 
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.HOST,
+    port: Number(process.env.PORT),
+    username: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    entities: [],
+    synchronize: true,
+  })],
+  controllers: [],
+  providers: [],
 })
-export class AppModule {}
+export class AppModule {
+
+}
